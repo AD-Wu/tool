e9d07eac13045c2709470f2efaaccb82102136c2
AD-Wu <944907103@qq.com>
2019-11-18 17:00:00 +0800

    完善

---
+++ src/main/java/com/x/commons/ssl/SSL.java
@@ -0,0 +1,125 @@
+package com.x.commons.ssl;
+
+import com.x.commons.ssl.enums.SSLVersion;
+import lombok.SneakyThrows;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.nio.conn.ssl.SSLIOSessionStrategy;
+import org.apache.http.ssl.SSLContexts;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.File;
+import java.io.FileInputStream;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+
+/**
+ * @Desc TODO
+ * @Date 2019-11-17 21:04
+ * @Author AD
+ */
+public class SSL {
+
+    private static final SSLVerifier VERIFIER = new SSLVerifier();
+
+    private final SSLSocketFactory sslFactory;
+
+    private final SSLConnectionSocketFactory sslConnFactory;
+
+    private final SSLIOSessionStrategy sslIOSessionStrategy;
+
+    // public SSL() {}
+
+    public SSL(SSLVersion version) {
+        SSLContext sslContext = getSSLContext(version);
+        initSSLContext(sslContext, VERIFIER, new SecureRandom());
+        this.sslFactory = sslContext.getSocketFactory();
+        this.sslConnFactory = new SSLConnectionSocketFactory(sslContext, VERIFIER);
+        this.sslIOSessionStrategy = new SSLIOSessionStrategy(sslContext, VERIFIER);
+    }
+
+    public SSL(String keyStorePath, String keyStorepass) {
+        SSLContext sslContext = getSSLContextWithKey(keyStorePath, keyStorepass);
+        initSSLContext(sslContext, VERIFIER, new SecureRandom());
+        this.sslFactory = sslContext.getSocketFactory();
+        this.sslConnFactory = new SSLConnectionSocketFactory(sslContext, VERIFIER);
+        this.sslIOSessionStrategy = new SSLIOSessionStrategy(sslContext, VERIFIER);
+    }
+
+    // @SneakyThrows
+    // public synchronized SSLSocketFactory getSSLSocketFactory(SSLVersion version) {
+    //     if (sslFactory != null) {
+    //         return sslFactory;
+    //     }
+    //     SSLContext context = getSSLContext(version);
+    //     initSSLContext(context, VERIFIER, null);
+    //     sslFactory = context.getSocketFactory();
+    //     return sslFactory;
+    // }
+    //
+    // @SneakyThrows
+    // public synchronized SSLConnectionSocketFactory getSSLConnSocketFactory(SSLVersion version) {
+    //     if (sslConnFactory != null) {
+    //         return sslConnFactory;
+    //     }
+    //     SSLContext context = getSSLContext(version);
+    //     initSSLContext(context, VERIFIER, new SecureRandom());
+    //     sslConnFactory = new SSLConnectionSocketFactory(context, VERIFIER);
+    //     return sslConnFactory;
+    // }
+    //
+    // @SneakyThrows
+    // public synchronized SSLIOSessionStrategy getSSLIOSessionStrategy(SSLVersion version) {
+    //     if (sslIOSessionStrategy != null) {
+    //         return sslIOSessionStrategy;
+    //     }
+    //     SSLContext context = getSSLContext(version);
+    //     initSSLContext(context, VERIFIER, new SecureRandom());
+    //     sslIOSessionStrategy = new SSLIOSessionStrategy(context, VERIFIER);
+    //     return sslIOSessionStrategy;
+    // }
+
+    /**
+     * TODO 自定义证书
+     *
+     * @param keyStorePath
+     * @param keyStorepass
+     */
+    @SneakyThrows
+    private SSLContext getSSLContextWithKey(String keyStorePath, String keyStorepass) {
+        String keyStoreType = KeyStore.getDefaultType();
+        KeyStore trustStore = KeyStore.getInstance(keyStoreType);
+        File keyStore = new File(keyStorePath);
+        try (FileInputStream in = new FileInputStream(keyStore);) {
+            trustStore.load(in, keyStorepass.toCharArray());
+            TrustSelfSignedStrategy strategy = new TrustSelfSignedStrategy();
+            // 相信自己的CA和所有自签名的证书
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(trustStore, strategy).build();
+            return sslContext;
+        }
+    }
+
+    @SneakyThrows
+    private SSLContext getSSLContext(SSLVersion version) {
+        return SSLContext.getInstance(version.get());
+    }
+
+    @SneakyThrows
+    private void initSSLContext(SSLContext sslContext, X509TrustManager verifier, SecureRandom secureRandom) {
+        sslContext.init(null, new TrustManager[]{VERIFIER}, secureRandom);
+    }
+
+    public SSLSocketFactory getSSLSocketFactory() {
+        return sslFactory;
+    }
+    public SSLConnectionSocketFactory getSSLConnSocketFactory() {
+        return sslConnFactory;
+    }
+    public SSLIOSessionStrategy getSSLIOSessionStrategy() {
+        return sslIOSessionStrategy;
+    }
+
+}

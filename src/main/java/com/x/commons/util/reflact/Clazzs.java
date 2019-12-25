2e58a9a48a993b7f53ea37c4600d09b012a1cfa0
AD-Wu <944907103@qq.com>
2019-12-25 14:14:07 +0800

    -

--- src/main/java/com/x/commons/util/reflact/Clazzs.java
+++ src/main/java/com/x/commons/util/reflact/Clazzs.java
@@ -16,53 +16,56 @@ import static java.util.stream.Collectors.toList;
  * @Author AD
  */
 public final class Clazzs {
-    
+
     /**
      * 类加载器
      */
     private static final ClassLoader LOADER = Clazzs.class.getClassLoader();
-    
+
     private Clazzs() {
     }
-    
+
+    public static ClassLoader getLoader() {
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        return loader == null ? LOADER : loader;
+    }
+
     public static <T> T newInstance(Class<T> clazz) throws Exception {
         Constructor<T> c = clazz.getDeclaredConstructor();
         c.setAccessible(true);
         return c.newInstance();
     }
-    
+
     public static boolean isPrimitive(Class<?> clazz) {
         return clazz == null ? false : clazz.isPrimitive();
     }
-    
+
     public static boolean isString(Class<?> clazz) {
         return String.class == clazz;
     }
-    
+
     public static boolean isObject(Class<?> clazz) {
         return !isPrimitive(clazz) && !isString(clazz) && !isArray(clazz);
     }
-    
+
     public static boolean isPrimitiveArray(Class<?> clazz) {
         return isArray(clazz) && isPrimitive(clazz.getComponentType());
     }
-    
+
     public static boolean isObjectArray(Class<?> clazz) {
         return isArray(clazz) && !isPrimitive(clazz.getComponentType());
     }
-    
+
     public static boolean isArray(Class<?> clazz) {
         return clazz == null ? false : clazz.isArray();
     }
-    
+
     /**
      * 获取包下带某个注解的类
      *
      * @param packageName 包名
      * @param annotation  注解类
-     *
      * @return
-     *
      * @author AD
      * @date 2018-12-22 23:37
      */
@@ -73,25 +76,23 @@ public final class Clazzs {
                 .filter(c -> c.getDeclaredAnnotation(annotation) != null)
                 .collect(toList());
     }
-    
+
     public static <A extends Annotation, I> List<Class<I>> getClass(String packageName, Class<A> annotation,
-            Class<I> interfaceClass) {
+                                                                    Class<I> interfaceClass) {
         List<Class<?>> collect = getClass(packageName, New.list());
         List<Class<I>> result = collect.stream()
                 .filter(c -> c.getDeclaredAnnotation(annotation) != null &&
-                             interfaceClass.isAssignableFrom(c))
+                        interfaceClass.isAssignableFrom(c))
                 .map(c -> (Class<I>) c).collect(toList());
         return result;
     }
-    
+
     /**
      * 获取某个包下的所有类
      *
      * @param packageName 包名
      * @param list
-     *
      * @return
-     *
      * @author AD
      * @date 2018-12-22 23:36
      */
@@ -99,19 +100,21 @@ public final class Clazzs {
     public static List<Class<?>> getClass(String packageName, List<Class<?>> list) {
         for (File f : Files.getFiles(packageName)) {
             if (f.isDirectory()) { getClass(getPath(packageName, f), list); }
-            if (f.getName().endsWith(".class")) { list.add(LOADER.loadClass(getClassName(packageName, f))); }
+            if (f.getName().endsWith(".class")) {
+                list.add(LOADER.loadClass(getClassName(packageName, f)));
+            }
         }
         return list;
     }
-    
+
     // ------------------------- 私有辅助方法 -------------------------
-    
+
     private static String getPath(String packageName, File file) {
         return packageName + "." + file.getName();
     }
-    
+
     private static String getClassName(String packageName, File file) {
         return getPath(packageName, file).replace(".class", "");
     }
-    
+
 }

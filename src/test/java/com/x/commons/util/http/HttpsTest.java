ce2df42182914c87c2fca93b9ede544cafae4063
AD-Wu <944907103@qq.com>
2020-01-06 16:11:44 +0800

    -

--- src/test/java/com/x/commons/util/http/HttpsTest.java
+++ src/test/java/com/x/commons/util/http/HttpsTest.java
@@ -3,6 +3,7 @@ package com.x.commons.util.http;
 import com.x.commons.enums.Charset;
 import com.x.commons.util.http.data.HttpResult;
 import com.x.commons.util.http.data.Json;
+import com.x.commons.util.string.Strings;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -11,7 +12,7 @@ import org.junit.jupiter.api.Test;
  * @Author AD
  */
 class HttpsTest {
-    
+
     @Test
     void get() throws Exception {
         String url = "http://localhost:8080/get";
@@ -22,7 +23,7 @@ class HttpsTest {
         System.out.println(resp);
         System.out.println(s);
     }
-  
+
     @Test
     void post() throws Exception {
         String url = "http://localhost:8080/post/json";
@@ -33,9 +34,9 @@ class HttpsTest {
         System.out.println(resp);
         System.out.println(s);
     }
-    
+
     @Test
-    void put() throws Exception{
+    void put() throws Exception {
         String url = "http://localhost:8080/put";
         Json json = new Json.Builder().of("user", "ad").of("pwd", "123").of("age", 12).build();
         HttpResult resp = Https.put(url, json);
@@ -44,9 +45,9 @@ class HttpsTest {
         System.out.println(resp);
         System.out.println(s);
     }
-    
+
     @Test
-    void delete() throws Exception{
+    void delete() throws Exception {
         String url = "http://localhost:8080/delete";
         Json json = new Json.Builder().of("user", "阳光").of("age", 12).build();
         HttpResult resp = Https.delete(url, json);
@@ -57,10 +58,16 @@ class HttpsTest {
     }
 
     @Test
-    void upload() throws Exception{
-        HttpResult upload = Https.upload("application.yml");
+    void upload() throws Exception {
+        String url = "http://localhost:8080/upload";
+        String[] paths = {"application.yml", "x-framework/x.yml"};
+        HttpResult upload = Https.upload(url, paths);
         System.out.println(upload);
+        String charset = upload.getCharset();
+        charset = Strings.isNull(charset) ? Charset.UTF8 : charset;
+        String s = new String(upload.getResult(), charset);
+        System.out.println(s);
     }
-   
-    
+
+
 }

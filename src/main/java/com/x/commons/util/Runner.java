ab7b71759eee2e67404aa151ee4211f8458755a3
AD-Wu <944907103@qq.com>
2020-01-08 10:10:19 +0800

    -

--- src/main/java/com/x/commons/util/Runner.java
+++ src/main/java/com/x/commons/util/Runner.java
@@ -12,12 +12,22 @@ public final class Runner {
 
     private static final int threadNum = 3;
 
-    private static final ExecutorService RUNNER = Executors.newFixedThreadPool(threadNum);
+    private static ExecutorService runner;
 
     private Runner() {}
 
     public static void add(Runnable runnable) {
-        RUNNER.submit(runnable);
+        if (runner != null) {
+            runner.submit(runnable);
+        } else {
+            synchronized (Runner.class) {
+                if (runner == null) {
+                    runner = Executors.newFixedThreadPool(threadNum);
+                }
+                runner.submit(runnable);
+            }
+        }
+
     }
 
 

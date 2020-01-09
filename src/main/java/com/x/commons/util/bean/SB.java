918ab1b6ec0f2c869f061337580a3a830f76bd8b
AD-Wu <944907103@qq.com>
2020-01-09 10:35:11 +0800

    -

--- src/main/java/com/x/commons/util/bean/SB.java
+++ src/main/java/com/x/commons/util/bean/SB.java
@@ -5,38 +5,38 @@ package com.x.commons.util.bean;
  * @Author AD
  */
 public final class SB {
-    
+
     private StringBuilder sb;
-    
+
     SB() {
         sb = new StringBuilder();
     }
-    
+
     SB(String s) {sb = new StringBuilder(s);}
-    
+
     public SB append(Object o) {
         sb.append(o);
         return this;
     }
-    
+
     public SB preAppend(Object o) {
         sb.insert(0, o);
         return this;
     }
-    
+
     public String sub(int start, int end) {
         return sb.substring(start, end);
     }
-    
+
     public String sub(int start) {
         return sb.substring(start);
     }
-    
+
     public SB insert(int offset, Object o) {
         sb.reverse();
         return this;
     }
-    
+
     public SB deleteFirst() {
         if (sb.length() == 0) {
             return this;
@@ -44,7 +44,7 @@ public final class SB {
         sb.deleteCharAt(0);
         return this;
     }
-    
+
     public SB deleteLast() {
         if (sb.length() == 0) {
             return this;
@@ -52,25 +52,17 @@ public final class SB {
         sb.deleteCharAt(sb.length() - 1);
         return this;
     }
-    
+
     public int length() {
         return sb.length();
     }
-    
+
     public String get() {
         return toString();
     }
-    
+
     public String toString() {
-        
         return sb.toString();
     }
-    
-    public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("ABCD");
-        sb.insert(0, 0).insert(0, 0);
-        System.out.println(sb.toString());
-    }
-    
+
 }

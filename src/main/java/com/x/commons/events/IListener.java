4b61326f054652097ce47dcb423636fb28d4de51
AD-Wu <944907103@qq.com>
2020-01-13 11:09:06 +0800

    -

---
+++ src/main/java/com/x/commons/events/IListener.java
@@ -0,0 +1,10 @@
+package com.x.commons.events;
+
+/**
+ * @Desc：
+ * @Author：AD
+ * @Date：2020/1/7 15:22
+ */
+public interface IListener {
+    void onEvent(Event event) throws Exception;
+}
--- src/main/java/com/x/commons/interfaces/ITarget.java
+++ src/main/java/com/x/commons/interfaces/ITarget.java
@@ -1,5 +1,7 @@
 package com.x.commons.interfaces;
 
+import com.x.commons.events.IListener;
+
 public interface ITarget<T> {
 
     void addListener(IListener<T> listener) throws Exception;

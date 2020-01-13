86162c188c0e4df7c76382a6b24887f74ce510de
AD-Wu <944907103@qq.com>
2020-01-13 17:17:00 +0800

    -

---
+++ src/main/java/com/x/framework/caching/datas/CacheEvent.java
@@ -0,0 +1,11 @@
+package com.x.framework.caching.datas;
+
+import com.x.commons.events.Event;
+
+/**
+ * @Desc：
+ * @Author：AD
+ * @Date：2020/1/13 14:34
+ */
+public class CacheEvent<T> extends Event {
+}

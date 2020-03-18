03c1748a90ba6811a8914133ad6a5ee51aeee98f
AD-Wu <944907103@qq.com>
2020-03-19 00:03:22 +0800

    -

--- src/main/java/com/x/protocol/core/Property.java
+++ src/main/java/com/x/protocol/core/Property.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Field;
  * @Date：2020/3/12 17:38
  */
 public class Property {
+    // ------------------------ 变量定义 ------------------------
     
     private String id;
     
@@ -32,5 +33,105 @@ public class Property {
     private Class<?> dataClass;
     
     private DataConfig dataConfig;
+    
+    // ------------------------ 构造方法 ------------------------
+    public Property() {}
+    // ------------------------ 方法定义 ------------------------
+    
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getSid() {
+        return sid;
+    }
+    
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+    
+    public int getMin() {
+        return min;
+    }
+    
+    public void setMin(int min) {
+        this.min = min;
+    }
+    
+    public int getMax() {
+        return max;
+    }
+    
+    public void setMax(int max) {
+        this.max = max;
+    }
+    
+    public int getLength() {
+        return length;
+    }
+    
+    public void setLength(int length) {
+        this.length = length;
+    }
+    
+    public String getLengthProp() {
+        return lengthProp;
+    }
+    
+    public void setLengthProp(String lengthProp) {
+        this.lengthProp = lengthProp;
+    }
+    
+    public boolean isPk() {
+        return pk;
+    }
+    
+    public void setPk(boolean pk) {
+        this.pk = pk;
+    }
+    
+    public String getDoc() {
+        return doc;
+    }
+    
+    public void setDoc(String doc) {
+        this.doc = doc;
+    }
+    
+    public Field getField() {
+        return field;
+    }
+    
+    public void setField(Field field) {
+        this.field = field;
+    }
+    
+    public Class<?> getType() {
+        return type;
+    }
+    
+    public void setType(Class<?> type) {
+        this.type = type;
+    }
+    
+    public Class<?> getDataClass() {
+        return dataClass;
+    }
+    
+    public void setDataClass(Class<?> dataClass) {
+        this.dataClass = dataClass;
+    }
+    
+    public DataConfig getDataConfig() {
+        return dataConfig;
+    }
+    
+    public void setDataConfig(DataConfig dataConfig) {
+        this.dataConfig = dataConfig;
+    }
     
 }
--- src/main/java/com/x/protocol/layers/application/Annotations.java
+++ src/main/java/com/x/protocol/layers/application/Annotations.java
@@ -1,6 +1,34 @@
 package com.x.protocol.layers.application;
 
-import java.lang.reflect.Type;
+import com.ax.framework.protocol.actor.models.CountResult;
+import com.ax.framework.protocol.actor.models.PageRequest;
+import com.ax.framework.protocol.actor.models.UpdateRequest;
+import com.ax.framework.protocol.actor.models.WhereRequest;
+import com.ax.protocol.annotations.AxActor;
+import com.ax.protocol.annotations.AxAttribute;
+import com.ax.protocol.annotations.AxData;
+import com.x.commons.collection.KeyValue;
+import com.x.commons.collection.NameValue;
+import com.x.commons.collection.Where;
+import com.x.commons.local.Locals;
+import com.x.commons.util.bean.New;
+import com.x.commons.util.collection.XArrays;
+import com.x.commons.util.reflact.Loader;
+import com.x.commons.util.string.Strings;
+import com.x.protocol.core.DataConfig;
+import com.x.protocol.core.Property;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.Serializable;
+import java.lang.reflect.*;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
+import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @Desc
@@ -8,18 +36,446 @@ import java.lang.reflect.Type;
  * @Author AD
  */
 public final class Annotations {
-
+    
     // ------------------------ 变量定义 ------------------------
-
+    
     // ------------------------ 构造方法 ------------------------
     private Annotations() {}
     // ------------------------ 方法定义 ------------------------
-
-    public static Class<?> getActualType(Class<?> clazz1, Class<?> clazz2,
-                                         Class<?> clazz3, Type type) {
-        return null;
+    
+    private static final FileFilter fileFilter = new FileFilter() {
+        
+        public boolean accept(File file) {
+            return file.isFile() ? file.getName().endsWith(".class") : true;
+        }
+    };
+    
+    public static void main(String[] args) throws Exception {
+        String className = Annotations.class.getName();
+        String pack = "com.x";
+        getAnnotationInfo(className, pack, false);
     }
-
+    
+    public static AnnotationInfo getAnnotationInfo(String className, String packageName, boolean var2) throws Exception {
+        if (packageName == null) packageName = "";
+        Class<?> clazz = !Strings.isNull(className) ? Class.forName(className) : null;
+        List<Class<?>> actors = New.list();
+        List<Class<?>> interfaces = New.list();
+        List<Class<?>> datas = New.list();
+        List<Class<?>> readyActors = New.list();
+        
+        ClassLoader loader = Loader.get();
+        String packagePath = packageName.replaceAll("\\.", "/");
+        Enumeration<URL> urlEnums = loader.getResources(packagePath);
+        if (urlEnums == null) {
+            URL[] urls = ((URLClassLoader) loader).getURLs();
+            for (URL url : urls) {
+                String path = url.getPath();
+                if (!path.endsWith("classes/")) {
+                    String fullPath = path + "!/" + packagePath;
+                    String[] jarPaths = fullPath.split("!");
+                    String jarPath = jarPaths[0].substring(jarPaths[0].indexOf("/"));
+                    try (JarFile jarFile = new JarFile(jarPath);) {
+                        handleJarEntity(jarFile, packagePath, clazz, actors, datas, readyActors, interfaces);
+                    } catch (Exception e) {
+                        throw e;
+                    }
+                }
+            }
+        } else {
+out:
+            while (true) {
+                URL url;
+                do {
+                    if (!urlEnums.hasMoreElements()) break out;
+                    url = urlEnums.nextElement();
+                } while (url == null);
+                String protocol = url.getProtocol();
+                if ("file".equals(protocol)) {
+                    getPackageClass(packageName, filterClassFiles(url.getPath()),
+                            clazz, actors, datas, readyActors, interfaces);
+                } else if ("jar".equals(protocol)) {
+                    JarURLConnection urlConn = (JarURLConnection) url.openConnection();
+                    
+                    try (JarFile jarFile = urlConn.getJarFile();) {
+                        handleJarEntity(jarFile, packagePath, clazz, actors, datas, readyActors, interfaces);
+                    } catch (Exception e) {
+                        throw e;
+                    }
+                }
+            }
+        }
+        addDefaultAnnotation(datas);
+        Map<Class<?>, DataConfig> dataConfigMap = New.map();
+        analyzeDatas(datas, dataConfigMap, var2);
+        unionDatas(dataConfigMap);
+        return new AnnotationInfo(actors, dataConfigMap, readyActors, interfaces);
+    }
+    
+    public static void checkError(Class<?> dataClazz, Object annotation, String annotationDoc, String var3, boolean var4)
+            throws Exception {
+        if (var4) {
+            if (var3 == null) {
+                var3 = "";
+            }
+            
+            if (var3.trim().length() < 1) {
+                throw new Exception(
+                        Locals.text("protocol.layer.annotation.err", annotationDoc,
+                                dataClazz.getName(),
+                                annotation.toString()));
+            }
+        }
+    }
+    
+    private static void handleJarEntity(JarFile jarFile, String packagePath, Class<?> clazz, List<Class<?>> actors,
+            List<Class<?>> datas, List<Class<?>> readyActors, List<Class<?>> interfaces) {
+        Enumeration<JarEntry> jarEntries = jarFile.entries();
+        
+        while (jarEntries.hasMoreElements()) {
+            JarEntry jarEntry = jarEntries.nextElement();
+            if (!jarEntry.isDirectory()) {
+                String jarName = jarEntry.getName();
+                if (jarName != null && jarName.length() >= 8) {
+                    if (jarName.charAt(0) == '/') {
+                        jarName = jarName.substring(1);
+                    }
+                    if (jarName.startsWith(packagePath) && jarName.endsWith(".class")) {
+                        jarName = jarName.replaceAll("/", ".");
+                        jarName = jarName.substring(0, jarName.length() - 6);
+                        analyzeClass(jarName, clazz, actors, datas, readyActors, interfaces);
+                    }
+                }
+            }
+        }
+    }
+    
+    private static void addDefaultAnnotation(List<Class<?>> datas) {
+        
+        if (datas.indexOf(KeyValue.class) == -1) {
+            datas.add(KeyValue.class);
+        }
+        
+        if (datas.indexOf(NameValue.class) == -1) {
+            datas.add(NameValue.class);
+        }
+        
+        if (datas.indexOf(Where.class) == -1) {
+            datas.add(Where.class);
+        }
+        
+        if (datas.indexOf(CountResult.class) == -1) {
+            datas.add(CountResult.class);
+        }
+        
+        if (datas.indexOf(PageRequest.class) == -1) {
+            datas.add(PageRequest.class);
+        }
+        
+        if (datas.indexOf(UpdateRequest.class) == -1) {
+            datas.add(UpdateRequest.class);
+        }
+        
+        if (datas.indexOf(WhereRequest.class) == -1) {
+            datas.add(WhereRequest.class);
+        }
+    }
+    
+    private static void analyzeDatas(List<Class<?>> datas, Map<Class<?>, DataConfig> dataConfigMap, boolean var2)
+            throws Exception {
+        Iterator<Class<?>> it = datas.iterator();
+        
+        while (it.hasNext()) {
+            Class<?> clazz = it.next();
+            AxData axData = clazz.getAnnotation(AxData.class);
+            checkError(clazz, axData, "doc", axData.doc(), var2);
+            String version = axData.version();
+            if (Strings.isNull(version)) {
+                version = "1";
+            }
+            
+            DataConfig dataConfig = new DataConfig();
+            dataConfig.setDataClass(clazz);
+            dataConfig.setDoc(axData.doc());
+            dataConfig.setVersion(version);
+            dataConfig.setCache(axData.caching());
+            dataConfig.setHistory(axData.history());
+            dataConfig.setTable(axData.table().trim());
+            List<String> pks = New.list();
+            List<Property> props = New.list();
+            analyzeProperties(clazz, props, pks, var2);
+            dataConfig.setPks((String[]) pks.toArray(new String[0]));
+            dataConfig.setProperties(props);
+            dataConfigMap.put(clazz, dataConfig);
+        }
+        
+    }
+    
+    private static void analyzeProperties(Class<?> dataClazz, List<Property> props, List<String> pks, boolean var3)
+            throws Exception {
+        Class superclass = dataClazz.getSuperclass();
+        if (superclass != null && superclass.isAnnotationPresent(AxData.class)) {
+            analyzeProperties(superclass, props, pks, var3);
+        }
+        
+        Field[] fields = dataClazz.getDeclaredFields();
+        if (!XArrays.isEmpty(fields)) {
+            for (int i = 0; i < fields.length; ++i) {
+                Field field = fields[i];
+                if (field.isAnnotationPresent(AxAttribute.class)) {
+                    AxAttribute axAttribute = field.getAnnotation(AxAttribute.class);
+                    checkError(dataClazz, axAttribute, "doc", axAttribute.doc(), var3);
+                    Class<?> fieldType = field.getType();
+                    Class dataClass = fieldType;
+                    if (!fieldType.isPrimitive() && !fieldType.equals(Object.class)) {
+                        if (fieldType.isAssignableFrom(List.class)) {
+                            Type genericType = field.getGenericType();
+                            if (genericType instanceof ParameterizedType) {
+                                ParameterizedType paramType = (ParameterizedType) genericType;
+                                Type[] actualTypes = paramType.getActualTypeArguments();
+                                if (actualTypes != null && actualTypes.length == 1) {
+                                    dataClass = (Class) actualTypes[0];
+                                }
+                            }
+                        } else if (fieldType.isArray()) {
+                            dataClass = fieldType.getComponentType();
+                        }
+                    }
+                    
+                    field.setAccessible(true);
+                    String fieldName = field.getName();
+                    Property prop = new Property();
+                    prop.setId(fieldName);
+                    prop.setSid(axAttribute.sid());
+                    prop.setMin(axAttribute.min());
+                    prop.setMax(axAttribute.max());
+                    prop.setLength(axAttribute.length());
+                    prop.setLengthProp(axAttribute.lengthProp());
+                    prop.setFormat(axAttribute.format());
+                    prop.setPk(axAttribute.pk());
+                    prop.setDoc(axAttribute.doc());
+                    prop.setField(field);
+                    prop.setType(fieldType);
+                    prop.setDataClass(dataClass);
+                    if (axAttribute.pk() && pks.indexOf(fieldName) == -1) {
+                        pks.add(fieldName);
+                    }
+                    
+                    props.add(prop);
+                }
+            }
+        }
+        
+    }
+    
+    public static Class<?> getActualType(Class<?> clazz, Class<?> var1, Class<?> var2, Type var3) {
+        if (var2.isPrimitive()) {
+            return var2;
+        } else {
+            Class var4;
+            if (var2.equals(Serializable.class)) {
+                var4 = getGenericType(var3);
+                if (var4 != null) {
+                    return var4;
+                }
+                
+                if (clazz.isInterface()) {
+                    Type[] var5 = clazz.getGenericInterfaces();
+                    if (var5 != null && var5.length > 0) {
+                        var4 = getGenericType(var5[0]);
+                        if (var4 != null) {
+                            return var4;
+                        }
+                    }
+                }
+                
+                var4 = getGenericType(var1.getGenericSuperclass());
+                if (var4 != null) {
+                    return var4;
+                }
+            } else if (var2.isArray()) {
+                var4 = var2.getComponentType();
+                if (var4.equals(Serializable.class)) {
+                    Class var10 = getGenericType(var3);
+                    if (var10 != null) {
+                        return var10;
+                    }
+                    
+                    if (clazz.isInterface()) {
+                        Type[] var6 = clazz.getGenericInterfaces();
+                        if (var6 != null && var6.length > 0) {
+                            var10 = getGenericType(var6[0]);
+                            if (var10 != null) {
+                                try {
+                                    return Class.forName("[L" + var10.getName() + ";");
+                                } catch (Exception var9) {
+                                    var9.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                    
+                    var10 = getGenericType(var1.getGenericSuperclass());
+                    if (var10 != null) {
+                        try {
+                            return Class.forName("[L" + var10.getName() + ";");
+                        } catch (Exception var8) {
+                            var8.printStackTrace();
+                        }
+                    }
+                }
+            }
+            
+            return var2;
+        }
+    }
+    
     // ------------------------ 私有方法 ------------------------
-
+    
+    private static Class<?> getGenericType(Type type) {
+        if (type == null) {
+            return null;
+        } else if (type instanceof ParameterizedType) {
+            return getGenericClass((ParameterizedType) type);
+        } else {
+            if (type instanceof GenericArrayType) {
+                Class clazz = getGenericType(((GenericArrayType) type).getGenericComponentType());
+                if (clazz == null) {
+                    return null;
+                }
+                
+                try {
+                    return Class.forName("[L" + clazz.getName() + ";");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (type instanceof TypeVariable) {
+                return getGenericType(((TypeVariable) type).getBounds()[0]);
+            }
+            
+            return null;
+        }
+    }
+    
+    private static Class<?> getGenericClass(ParameterizedType paramType) {
+        Type[] types = paramType.getActualTypeArguments();
+        if (!XArrays.isEmpty(types)) {
+            Type type = types[0];
+            if (type instanceof ParameterizedType) {
+                return (Class) ((ParameterizedType) type).getRawType();
+            } else {
+                return type instanceof TypeVariable ? getGenericType(
+                        ((TypeVariable) type).getBounds()[0]) : (Class) type;
+            }
+        } else {
+            return null;
+        }
+    }
+    
+    private static void unionDatas(Map<Class<?>, DataConfig> dataConfigMap) {
+        Iterator<DataConfig> it = dataConfigMap.values().iterator();
+        
+        while (it.hasNext()) {
+            DataConfig dataConfig = it.next();
+            Property[] props = dataConfig.getProperties();
+            int len = props.length;
+            for (int i = 0; i < len; ++i) {
+                Property prop = props[i];
+                Class<?> clazz = prop.getDataClass();
+                if (dataConfigMap.containsKey(clazz)) {
+                    prop.setDataConfig(dataConfigMap.get(clazz));
+                }
+            }
+        }
+        
+    }
+    
+    private static File[] filterClassFiles(String path) {
+        return Strings.isNull(path) ? null : (new File(path)).listFiles(fileFilter);
+    }
+    
+    private static String getClassName(String var0, String var1) {
+        if (var1 == null) {
+            return null;
+        } else {
+            int var2 = var1.lastIndexOf(".");
+            return var2 <= 0 ? null : var0 + "." + var1.substring(0, var2);
+        }
+    }
+    
+    private static void analyzeClass(String className, Class<?> var1, List<Class<?>> var2, List<Class<?>> datas,
+            List<Class<?>> var4, List<Class<?>> actors) {
+        if (!Strings.isNull(className)) {
+            Class<?> clazz = null;
+            
+            try {
+                clazz = Class.forName(className);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
+            if (clazz != null) {
+                if (clazz.isAnnotationPresent(AxData.class)) {
+                    datas.add(clazz);
+                } else {
+                    if (clazz.isAnnotationPresent(AxActor.class)) {
+                        if (clazz.isInterface()) {
+                            actors.add(clazz);
+                        } else if (var1 == null) {
+                            var2.add(clazz);
+                        } else if (!var1.equals(clazz) && var1.isAssignableFrom(clazz)) {
+                            var2.add(clazz);
+                        }
+                    }
+                    
+                    if (var1 == null) {
+                        try {
+                            if (clazz.getInterfaces().length == 0) {
+                                return;
+                            }
+                            
+                            if (clazz.getConstructors().length == 0) {
+                                return;
+                            }
+                            
+                            var4.add(clazz);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else if (!var1.equals(clazz) && var1.isAssignableFrom(clazz)) {
+                        try {
+                            if (clazz.getConstructors().length == 0) {
+                                return;
+                            }
+                            
+                            var4.add(clazz);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    
+                }
+            }
+        }
+    }
+    
+    private static void getPackageClass(String packageName, File[] files, Class<?> var2, List<Class<?>> var3,
+            List<Class<?>> var4, List<Class<?>> var5, List<Class<?>> var6) {
+        if (!XArrays.isEmpty(files)) {
+            
+            for (int i = 0, L = files.length; i < L; ++i) {
+                File file = files[i];
+                String fileName = file.getName();
+                if (file.isFile()) {
+                    analyzeClass(getClassName(packageName, fileName), var2, var3, var4, var5, var6);
+                } else {
+                    String var12 = packageName.length() > 0 ? packageName + "." + fileName : fileName;
+                    getPackageClass(var12, filterClassFiles(file.getPath()), var2, var3, var4,
+                            var5, var6);
+                }
+            }
+            
+        }
+    }
+    
 }

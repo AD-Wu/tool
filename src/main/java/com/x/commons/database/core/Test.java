99ae4e1238014989034191d07723c7c045e53327
AD-Wu <944907103@qq.com>
2020-03-24 00:13:07 +0800

    -

--- src/main/java/com/x/commons/database/core/Test.java
+++ src/main/java/com/x/commons/database/core/Test.java
@@ -1,5 +1,6 @@
 package com.x.commons.database.core;
 
+import com.x.commons.collection.Where;
 import com.x.commons.util.file.Files;
 import com.x.framework.database.DaoManager;
 import com.x.framework.database.IDao;
@@ -28,5 +29,19 @@ public class Test {
         dao.add(user);
         User old = dao.getByPrimary(user.getId());
         System.out.println(old);
+        Where[] ws = new Where[]{
+                new Where("name", "=", "AD")
+        };
+        User bean = dao.getBean(ws);
+        System.out.println(bean);
+        bean.setAge(38);
+        int edit = dao.edit(bean);
+        System.out.println("edit="+edit);
+        bean.setSex(false);
+        User put = dao.put(bean);
+        System.out.println("put");
+        System.out.println(put);
+        int delete = dao.delete(new String[]{"id"}, new Object[]{"123"});
+        System.out.println("delete="+delete);
     }
 }

0a04dabd0fb93bdc4b6f6ad823690dc32eeed6d4
AD-Wu <944907103@qq.com>
2020-04-27 16:32:17 +0800

    -

--- src/main/java/com/x/commons/socket/test/Test.java
+++ src/main/java/com/x/commons/socket/test/Test.java
@@ -4,6 +4,9 @@ import com.x.commons.socket.bean.SocketConfig;
 import com.x.commons.socket.bean.XSocketSerializer;
 import com.x.commons.socket.server.SocketServer;
 
+import java.net.InetAddress;
+import java.net.Socket;
+
 /**
  * @Desc TODO
  * @Date 2020-02-18 20:03
@@ -17,8 +20,15 @@ public class Test {
         SocketServer server = new SocketServer(SocketConfig.server(7777), listener,serializer);
         server.start();
         System.out.println("服务启动成功");
-    
-        
+
+        Socket client = new Socket("localhost", 7777, InetAddress.getByName("localhost"), 8888);
+        String s = client.getLocalAddress().toString();
+        int localPort = client.getLocalPort();
+        System.out.println(s+":"+localPort);
+        System.out.println(client.getLocalSocketAddress().toString());
+
     }
+
+
     
 }

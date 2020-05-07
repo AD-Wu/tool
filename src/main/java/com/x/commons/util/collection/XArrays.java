7c0efa9e14ae99126ded3c12467ce33826e19232
AD-Wu <944907103@qq.com>
2020-05-07 15:10:52 +0800

    -

--- src/main/java/com/x/commons/util/collection/XArrays.java
+++ src/main/java/com/x/commons/util/collection/XArrays.java
@@ -6,6 +6,7 @@ import com.x.commons.util.reflact.Clazzs;
 
 import java.io.File;
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,55 +17,55 @@ import java.util.stream.Stream;
  * @Author AD
  */
 public final class XArrays {
-    
+
     /**
      * 空 byte[] 数组
      */
     public static final byte[] EMPTY_BYTE = new byte[0];
-    
+
     /**
      * 空 short[] 数组
      */
     public static final short[] EMPTY_SHORT = new short[0];
-    
+
     /**
      * 空 int[] 数组
      */
     public static final int[] EMPTY_INT = new int[0];
-    
+
     /**
      * 空 long[] 数组
      */
     public static final long[] EMPTY_LONG = new long[0];
-    
+
     /**
      * 空 float[] 数组
      */
     public static final float[] EMPTY_FLOAT = new float[0];
-    
+
     /**
      * 空 double[] 数组
      */
     public static final double[] EMPTY_DOUBLE = new double[0];
-    
+
     /**
      * 空 String[] 数组
      */
     public static final String[] EMPTY_STRING = new String[0];
-    
+
     /**
      * 空 File[] 数组
      */
     public static final File[] EMPTY_FILE = new File[0];
-    
+
     /**
      * 空 Object[] 数组
      */
     public static final Object[] EMPTY = new Object[0];
-    
+
     private XArrays() {
     }
-    
+
     public static byte[] copy(byte[] data) {
         if (!isEmpty(data)) {
             byte[] copy = new byte[data.length];
@@ -73,58 +74,58 @@ public final class XArrays {
         }
         return EMPTY_BYTE;
     }
-    
+
     public static boolean isValid(Object[] os1, Object[] os2) {
         if (!isEmpty(os1) && !isEmpty(os2) && os1.length == os2.length) {
             return true;
         }
         return false;
     }
-    
+
     public static <T> boolean isEmpty(Set<T> set) {
         return set == null || set.size() == 0;
     }
-    
+
     public static <T> boolean isEmpty(List<T> list) {
         return list == null || list.size() == 0;
     }
-    
+
     public static <T> boolean isEmpty(T[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(int[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(byte[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(short[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(long[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(float[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(double[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static boolean isEmpty(char[] arrays) {
         return arrays == null || arrays.length == 0;
     }
-    
+
     public static <T> Set<T> toSet(T[] ts) {
         return Stream.of(ts).collect(Collectors.toSet());
     }
-    
+
     public static <T> T[] toArray(List<T> list, Class<T> clazz) {
         if (isEmpty(list)) {
             return null;
@@ -132,14 +133,26 @@ public final class XArrays {
         T[] ts = (T[]) Array.newInstance(clazz, list.size());
         return list.toArray(ts);
     }
-    
+
+    public static <T> List<T> join(T[] arr1, T[] arr2) {
+        if (arr1 == null) {
+            return Arrays.asList(arr2);
+        }
+        if (arr2 == null) {
+            return Arrays.asList(arr1);
+        }
+        List<T> list = New.list(arr1.length + arr2.length);
+        list.addAll(Arrays.asList(arr1));
+        list.addAll(Arrays.asList(arr2));
+        return list;
+    }
+
     /**
      * 对象型数组和基本数据类型数组进行互转，如：byte[] <-> Byte[]
      *
      * @param value
      * @param targetClasses 目标类型,可选参数，为null时自动互换，如：byte[] <-> Byte[]；<br>
      *                      不为null时则只取第一个
-     *
      * @return
      */
     public static Object convert(Object value, Class<?>... targetClasses) {
@@ -159,12 +172,11 @@ public final class XArrays {
         }
         return convert(value);
     }
-    
+
     /**
      * 对象型数组和基本数据类型数组进行互转，如：byte[] <-> Byte[]
      *
      * @param value 数组（基本数据类型或所对应的对象类型）
-     *
      * @return
      */
     private static Object convert(Object value) {
@@ -323,21 +335,21 @@ public final class XArrays {
             return instance;
         }
     }
-    
+
     public static void main(String[] args) {
         int[] is = {1, 2, 3};
-        
+
         System.out.println("is=" + is.getClass());
         Object convert = convert(is);
         System.out.println("is after convert=" + convert.getClass());
-        
+
         Integer[] ints = new Integer[]{1, 2, 3};
         System.out.println("ints=" + ints.getClass());
         Object convert1 = convert(ints);
         System.out.println("ints after convert=" + convert1.getClass());
         boolean empty = isEmpty(New.list());
         System.out.println(empty);
-        
+
     }
-    
+
 }
--- src/main/java/com/x/commons/util/reflact/Fields.java
+++ src/main/java/com/x/commons/util/reflact/Fields.java
@@ -1,11 +1,11 @@
 package com.x.commons.util.reflact;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.x.commons.util.collection.XArrays;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Properties;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -19,17 +19,59 @@ public final class Fields {
     private static final Field[] EMPTY_FIELDS = new Field[0];
 
     private Fields() {}
+
     public static Field[] getFields(Class<?> target) {
         return target.getDeclaredFields();
     }
 
     public static Field[] getFields(Class<?> target, Class<? extends Annotation> annotation) {
         final List<Field> fields =
-                Stream.of(target.getDeclaredFields()).filter(f -> f.isAnnotationPresent(annotation)).collect(toList());
+                Stream.of(target.getDeclaredFields()).filter(
+                        f -> f.isAnnotationPresent(annotation)).collect(toList());
         return fields.isEmpty() ? EMPTY_FIELDS : fields.toArray(EMPTY_FIELDS);
     }
 
+    public static Field[] getFields(Class<?> target, boolean parent) {
+        if (target == null) {
+            return EMPTY_FIELDS;
+        }
+        // 获取target类的public、protected、default、private的所有属性
+        Field[] fields = target.getDeclaredFields();
+        if (parent) {
+            Field[] superFields = getFields(target.getSuperclass(), parent);
+            return XArrays.join(fields, superFields).toArray(new Field[0]);
+        }
+        return fields;
+    }
 
+    public static void main(String[] args) {
+        Field[] fields = getFields(Subb.class, true);
+        for (Field field : fields) {
+            System.out.println(field.getName());
+        }
+    }
 
+    public static class Parent {
+        public String name;
+
+        protected int age;
+
+        boolean sex;
+
+        private LocalDateTime birthday;
+    }
+
+    public static class Sub extends Parent {
+        public String addr;
+
+        protected int number;
+
+        int count;
+
+        private boolean rich;
+    }
+    public static class Subb extends Sub{
+        private String test;
+    }
 }
 

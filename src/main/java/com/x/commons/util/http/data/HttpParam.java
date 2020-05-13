4b89beadf0e6d607ea71a3a59a163ce595fcccda
AD-Wu <944907103@qq.com>
2020-05-13 16:44:10 +0800

    -

--- src/main/java/com/x/commons/util/http/data/HttpParam.java
+++ src/main/java/com/x/commons/util/http/data/HttpParam.java
@@ -2,6 +2,7 @@ package com.x.commons.util.http.data;
 
 import com.x.commons.util.bean.New;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -12,42 +13,47 @@ import java.util.Map;
  */
 public final class HttpParam implements Iterable<Map.Entry<ValueType, Object>> {
     // ------------------------ 变量定义 ------------------------
-    
+
     private final Map<ValueType, Object> params;
-    
+
     // ------------------------ 构造方法 ------------------------
-    
+
     public HttpParam() {
         this.params = New.linkedMap();
     }
-    
+
     // ------------------------ 方法定义 ------------------------
     public HttpParam add(Object key, Object value) {
-        ValueType<Object> type = new ValueType<>(ValueType.OBJECT, key);
-        params.put(type,value);
-        return this;
+        Class<?> clazz = value.getClass();
+        if (clazz.equals(byte[].class)) {
+            return add(ValueType.BYTES, key, (byte[]) value);
+        } else if (clazz.equals(File.class)) {
+            return add(ValueType.FILE, key, (File) value);
+        } else {
+            return add(ValueType.OBJECT, key, value);
+        }
     }
-    
+
     public <T> HttpParam add(ValueType<T> type, Object key, T value) {
-        params.put(new ValueType<>(type, key),value);
+        params.put(new ValueType<>(type, key), value);
         return this;
     }
-    
+
     public String toJson() {
         return getJson().toJson();
     }
-    
+
     public String toKeyValue() {
         return getJson().toKeyValue();
     }
-    
+
     @Override
     public Iterator<Map.Entry<ValueType, Object>> iterator() {
         return params.entrySet().iterator();
     }
-    
+
     // ------------------------ 私有方法 ------------------------
-    
+
     private Json getJson() {
         Iterator<Map.Entry<ValueType, Object>> it = params.entrySet().iterator();
         Json json = new Json();
@@ -59,5 +65,5 @@ public final class HttpParam implements Iterable<Map.Entry<ValueType, Object>> {
         }
         return json;
     }
-   
+
 }
